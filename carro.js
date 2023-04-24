1b5e55d7287463a9865fdcd1ce5b73ab0bad4563
apePetrus <ShiveringVeteran@proton.me>
2023-04-23 22:12:13 -0300

    freeway
    A clone from the classic Freeway game.

---
+++ carro.js
@@ -0,0 +1,39 @@
+//conteúdos dos carros
+
+/*
+Listas buscam valores que você insere nelas.
+Nesse caso, se eu chamar yCarro[0], vou receber 40
+como resultado (sempre é iniciado em 0).
+[1] será 96 e por aí vai.
+*/
+
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318];
+let velocidadeCarros = [2, 2.5, 3, 5, 3.3, 2.3];
+
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+  for(let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for(let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarro){
+  return xCarro < -50;
+}
